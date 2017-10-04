d1f08a55af2e3a8c4b6fcd6fe6b2bd04ae75ea6b
Mark Faga <mark.faga@ezcater.com>
2017-10-04 07:09:55 -0400

    Add RspecRequireBrowserMock Cop

--- lib/ezcater_rubocop.rb
+++ lib/ezcater_rubocop.rb
@@ -1,5 +1,6 @@
 require "ezcater_rubocop/version"
 require "rubocop-rspec"
+require "string_manipulator"
 
 # Because RuboCop doesn't yet support plugins, we have to monkey patch in a
 # bit of our configuration. Based on approach from rubocop-rspec:
@@ -12,6 +13,7 @@ puts "configuration from #{DEFAULT_FILES}" if RuboCop::ConfigLoader.debug?
 config = RuboCop::ConfigLoader.merge_with_default(config, path)
 RuboCop::ConfigLoader.instance_variable_set(:@default_configuration, config)
 
+require "rubocop/cop/ezcater/rspec_require_browser_mock"
 require "rubocop/cop/ezcater/rspec_require_feature_flag_mock"
 require "rubocop/cop/ezcater/rspec_dot_not_self_dot"
 require "rubocop/cop/ezcater/style_dig"
---
+++ lib/rubocop/cop/ezcater/rspec_require_browser_mock.rb
@@ -0,0 +1,56 @@
+module RuboCop
+  module Cop
+    module Ezcater
+      # Enforce use of `mock_ezcater_app`, `mock_chrome_browser` & `mock_custom_browser` helpers
+      # instead of mocking `Browser` or `EzBrowser` directly.
+      #
+      # @example
+      #
+      #   # good
+      #   mock_ezcater_app
+      #   mock_chrome_browser
+      #   mock_custom_browser
+      #   mock_ezcater_app(device: "My Device", version: "1.2.3", language: "en=US,en")
+      #   mock_chrome_browser(device: "My Device", version: "1.2.3", language: "en=US,en")
+      #   mock_custom_browser(user_agent: "My Custom Agent", language: "en=US,en"
+      #
+      #   # bad
+      #   allow(Browser).to receive...
+      #   allow(EzBrowser).to receive...
+      class RspecRequireBrowserMock < Cop
+        EZ_BROWSER_MSG = "Use the mocks provided by `BrowserHelpers` instead of mocking `allow(EzBrowser)`".freeze
+        BROWSER_MSG = "Use the mocks provided by `BrowserHelpers` instead of mocking `allow(Browser)`".freeze
+
+        def_node_matcher :browser_const?, <<~PATTERN
+          (const _ {:Browser, :EzBrowser})
+        PATTERN
+
+        def on_const(node)
+          return unless browser_const?(node)
+
+          # Walk to send node where method = :allow
+          match_node = node
+          match_node = match_node.parent while not_allow_send_node?(match_node.parent)
+          match_node = match_node.parent
+
+          # Validate send node, method = :allow; could have never been found
+          return unless allow_send_node?(match_node)
+
+          # Finish tree navigation to full line for highlighting
+          match_node = match_node.parent while match_node.parent
+          add_offense(match_node, :expression, RspecRequireBrowserMock.const_get("#{StringManipulator.underscore(node.source).upcase}_MSG"))
+        end
+
+        private
+
+        def not_allow_send_node?(node)
+          node && !allow_send_node?(node)
+        end
+
+        def allow_send_node?(node)
+          node && node.send_type? && node.method_name == :allow
+        end
+      end
+    end
+  end
+end
