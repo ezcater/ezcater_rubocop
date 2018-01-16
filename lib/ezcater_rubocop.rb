b1a856f0a987024d8a055f9065c1651875f49c74
Tim Perkins <tim.perkins@ezcater.com>
2018-01-16 13:39:36 -0500

    Add RailsConfiguration cop

--- lib/ezcater_rubocop.rb
+++ lib/ezcater_rubocop.rb
@@ -13,6 +13,7 @@ config = RuboCop::ConfigLoader.merge_with_default(config, path)
 RuboCop::ConfigLoader.instance_variable_set(:@default_configuration, config)
 
 require "rubocop/cop/ezcater/private_attr"
+require "rubocop/cop/ezcater/rails_configuration"
 require "rubocop/cop/ezcater/rspec_require_browser_mock"
 require "rubocop/cop/ezcater/rspec_require_feature_flag_mock"
 require "rubocop/cop/ezcater/rspec_require_gql_error_helpers"
