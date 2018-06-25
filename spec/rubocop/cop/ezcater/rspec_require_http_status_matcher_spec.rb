f1c407b02d98f3c2ff44794fda46ff678b25f19a
Brent Wheeldon <brent.wheeldon@gmail.com>
2018-06-25 17:46:51 -0400

    Add a cop to enforce response status assertion style

---
+++ spec/rubocop/cop/ezcater/rspec_require_http_status_matcher_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe RuboCop::Cop::Ezcater::RspecRequireHttpStatusMatcher do
+  subject(:cop) { described_class.new }
+
+  it "accepts usage of the HTTP status matcher" do
+    inspect_source("expect(response).to have_http_status :bad_request")
+    expect(cop.offenses).to be_empty
+  end
+
+  it "registers an offence when attempting to assert directly on the status" do
+    inspect_source("expect(response.code).to eq 400")
+    expect(cop.messages).to match_array(["Use the `have_http_status` matcher, like "\
+      "`expect(response).to have_http_status :bad_request`, rather than `expect(response.code).to eq 400`",])
+  end
+end
