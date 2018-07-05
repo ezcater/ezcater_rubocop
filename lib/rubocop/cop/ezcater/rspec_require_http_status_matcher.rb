93f180b358e48b73e5fb43577208f25aa329f56d
Brent Wheeldon <brent.wheeldon@gmail.com>
2018-07-05 15:17:11 -0400

    Fix `Ezcater/RspecRequireHttpStatusMatcher` cop
    Previously this was looking for `response.code` being equated with an integer which isn’t how these specs work - it will be being equated with a string. It also wasn’t looking for, and banning, `response.status` being compared with an integer.

--- lib/rubocop/cop/ezcater/rspec_require_http_status_matcher.rb
+++ lib/rubocop/cop/ezcater/rspec_require_http_status_matcher.rb
@@ -18,12 +18,16 @@ module RuboCop
         MSG = "Use the `have_http_status` matcher, like `expect(response).to have_http_status :bad_request`, "\
           "rather than `%<node_source>s`"
 
+        def_node_matcher :response_status_assertion, <<~PATTERN
+          (send (send _ :expect (send (send _ :response) :status)) :to (send _ :eq (int _)))
+        PATTERN
+
         def_node_matcher :response_code_assertion, <<~PATTERN
-          (send (send _ :expect (send (send _ :response) :code)) :to (send _ :eq (int _)))
+          (send (send _ :expect (send (send _ :response) :code)) :to (send _ :eq (str _)))
         PATTERN
 
         def on_send(node)
-          return unless response_code_assertion(node)
+          return if !response_status_assertion(node) && !response_code_assertion(node)
 
           add_offense(node,
                       location: :expression,
