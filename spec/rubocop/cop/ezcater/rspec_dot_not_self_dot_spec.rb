5752e74c78542685ce98de863d3d090525e302ff
Julian Jurai <andrewjurai@gmail.com>
2018-10-05 11:10:32 -0400

    Add spec

--- spec/rubocop/cop/ezcater/rspec_dot_not_self_dot_spec.rb
+++ spec/rubocop/cop/ezcater/rspec_dot_not_self_dot_spec.rb
@@ -4,14 +4,15 @@
 RSpec.describe RuboCop::Cop::Ezcater::RspecDotNotSelfDot, :config do
   subject(:cop) { described_class.new(config) }
 
-  let(:msgs) { [described_class::MSG] }
+  let(:self_dot_msg) { [described_class::SELF_DOT_MSG] }
+  let(:colon_colon_msg) { [described_class::COLON_COLON_REGEXP] }
 
   RuboCop::RSpec::Language::ExampleGroups::ALL.each do |name|
     it "corrects #{name} with `self.class_method`" do
       source = "#{name} \"self.class_method\" do\nend"
       inspect_source(source)
       expect(cop.highlights).to match_array(['"self.class_method"'])
-      expect(cop.messages).to match_array(msgs)
+      expect(cop.messages).to match_array(self_dot_msg)
       expect(autocorrect_source(source)).to eq(source.sub("self.", "."))
     end
 
@@ -19,8 +20,24 @@ RSpec.describe RuboCop::Cop::Ezcater::RspecDotNotSelfDot, :config do
       source = "#{name} \"self.class_method\", foo: true do\nend"
       inspect_source(source)
       expect(cop.highlights).to match_array(['"self.class_method"'])
-      expect(cop.messages).to match_array(msgs)
+      expect(cop.messages).to match_array(self_dot_msg)
       expect(autocorrect_source(source)).to eq(source.sub("self.", "."))
+    end
+
+    it "corrects #{name} with `::class_method`" do
+      source = "#{name} \"::class_method\" do\nend"
+      inspect_source(source)
+      expect(cop.highlights).to match_array(['"::class_method"'])
+      expect(cop.messages).to match_array(colon_colon_msg)
+      expect(autocorrect_source(source)).to eq(source.sub("::", "."))
+    end
+
+    it "corrects #{name} with `::class_method` and metadata" do
+      source = "#{name} \"::class_method\", foo: true do\nend"
+      inspect_source(source)
+      expect(cop.highlights).to match_array(['"::class_method"'])
+      expect(cop.messages).to match_array(colon_colon_msg)
+      expect(autocorrect_source(source)).to eq(source.sub("::", "."))
     end
 
     it "accepts #{name} with `.class_method`" do
