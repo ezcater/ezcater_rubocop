2d7ec80f43be612d154ad030ca934b17422f8b3c
Adam Prescott <adam.prescott@ezcater.com>
2019-09-19 15:53:55 -0400

    Add a cop to detect AR::Base.connection.execute in Rails migrations
    A migration can run SQL directly using
    `ActiveRecord::Base.connection.execute`, but not only is this redundant
    it removes strong_migrations' ability to detect unsafe migrations,
    meaning there's no check that `safety_assured` is in use. This cop
    should push us to using `execute` directly.

--- lib/ezcater_rubocop.rb
+++ lib/ezcater_rubocop.rb
@@ -17,6 +17,7 @@ RuboCop::ConfigLoader.instance_variable_set(:@default_configuration, config)
 require "rubocop/cop/ezcater/direct_env_check"
 require "rubocop/cop/ezcater/rails_configuration"
 require "rubocop/cop/ezcater/rails_env"
+require "rubocop/cop/ezcater/rails_top_level_sql_execute"
 require "rubocop/cop/ezcater/require_gql_error_helpers"
 require "rubocop/cop/ezcater/rspec_match_ordered_array"
 require "rubocop/cop/ezcater/rspec_require_browser_mock"
---
+++ spec/rubocop/cop/ezcater/rails_top_level_sql_execute_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::Ezcater::RailsTopLevelSqlExecute do
+  subject(:cop) { described_class.new }
+
+  let(:msgs) { [described_class::MSG] }
+
+  it "accepts `execute`" do
+    source = "execute('SELECT * FROM foo')"
+    inspect_source(source)
+    expect(cop.offenses).to be_empty
+  end
+
+  it "detects `ActiveRecord::Base.connection.execute`" do
+    source = "ActiveRecord::Base.connection.execute('SELECT * FROM foo')"
+    inspect_source(source)
+    expect(cop.offenses).not_to be_empty
+    expect(cop.highlights).to match_array([source])
+    expect(cop.messages).to match_array(msgs)
+  end
+
+  it "detects `ActiveRecord::Base.connection.execute` with non-string arguments" do
+    source = "ActiveRecord::Base.connection.execute(foo_bar)"
+    inspect_source(source)
+    expect(cop.offenses).not_to be_empty
+    expect(cop.highlights).to match_array([source])
+    expect(cop.messages).to match_array(msgs)
+  end
+
+  it "supports autocorrect" do
+    source = "ActiveRecord::Base.connection.execute('SELECT * FROM foo')"
+    inspect_source(source)
+    expect(cop.highlights).to match_array([source])
+    expect(cop.messages).to match_array(msgs)
+    expect(autocorrect_source(source)).to eq("execute('SELECT * FROM foo')")
+  end
+end
