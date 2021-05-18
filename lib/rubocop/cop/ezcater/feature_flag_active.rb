845c5fb6e3ff546376f8e0abcc6a208093a612e0
Michael Berkowitz <michael.berkowitz@gmail.com>
2021-05-18 11:10:32 -0400

    Add cop for ensuring `EzFF.active?` is called with at least one of the
    required named arguments to go with our upcoming 2.0.0 release
    of the `ezcater_feature_flag-client-ruby` gem

---
+++ lib/rubocop/cop/ezcater/feature_flag_active.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Ezcater
+      # Use `EzcaterFeatureFlag.active?` with a tracking_id or array of identifiers
+      #
+      # @example
+      #
+      #   # good
+      #   EzFF.active?("FlagName", tracking_id: "user:12345")
+      #   EzFF.active?("FlagName", identifiers: ["user:12345", "user:23456"])
+      #
+      #   # bad
+      #   EzFF.active?("FlagName")
+
+      class FeatureFlagActive < Cop
+
+        MSG = "`EzFF.active?` must be called with at least one of `tracking_id` or `identifiers`"
+
+        def_node_matcher :ezff_active_one_arg, <<-PATTERN
+          (send
+            (_ _ {:EzFF :EzcaterFeatureFlag}) :active? (str _))
+        PATTERN
+
+        def_node_matcher :args_matcher, <<-PATTERN
+          (send
+            (_ _ {:EzFF :EzcaterFeatureFlag}) :active?
+              (str _)
+              (_
+                (pair
+                  (sym {:tracking_id :identifiers})
+                  _)
+                ...))
+        PATTERN
+
+        def on_send(node)
+          if ezff_active_one_arg(node) || !args_matcher(node)
+            add_offense(node, location: :expression, message: MSG)
+          end
+        end
+      end
+    end
+  end
+end
