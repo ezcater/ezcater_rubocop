ff5002007d67bb5eaf0c577bbba5f359ddbd90f6
Michael Berkowitz <michael.berkowitz@gmail.com>
2021-05-19 15:48:38 -0400

    Add fix for FeatureFlagActive cop
    The previous logic was overly broad in what it would flag. Anything
    that parsed to use `send` in the AST but wasn't a call to
    `EzFF.active?` would be flagged an invalid. This includes things like
    `require` and `.config do` blocks.

--- lib/rubocop/cop/ezcater/feature_flag_active.rb
+++ lib/rubocop/cop/ezcater/feature_flag_active.rb
@@ -15,7 +15,6 @@ module RuboCop
       #   EzFF.active?("FlagName")
 
       class FeatureFlagActive < Cop
-
         MSG = "`EzFF.active?` must be called with at least one of `tracking_id` or `identifiers`"
 
         def_node_matcher :ezff_active_one_arg, <<-PATTERN
@@ -34,7 +33,14 @@ module RuboCop
                 ...))
         PATTERN
 
+        def_node_matcher :method_call_matcher, <<-PATTERN
+          (send
+            (_ _ {:EzFF :EzcaterFeatureFlag}) :active? ...)
+        PATTERN
+
         def on_send(node)
+          return unless method_call_matcher(node)
+
           if ezff_active_one_arg(node) || !args_matcher(node)
             add_offense(node, location: :expression, message: MSG)
           end
