76afd5ad4b9a7ab6abd25e54ea07ab8c5deeaa25
Jon Sagotsky <jon.sagotsky@ezcater.com>
2021-06-04 10:09:06 -0400

    Bump ruby version in test to minimum supported by gem
    This gem requires 2.6+.  The 2.3 in this spec is no longer supported and
    so it would not run.

--- spec/rubocop/cop/ezcater/style_dig_spec.rb
+++ spec/rubocop/cop/ezcater/style_dig_spec.rb
@@ -4,7 +4,7 @@
 RSpec.describe RuboCop::Cop::Ezcater::StyleDig, :config do
   subject(:cop) { described_class.new(config) }
 
-  let(:ruby_version) { 2.3 }
+  let(:ruby_version) { 2.6 }
   let(:msgs) { [described_class::MSG] }
 
   it "accepts non-nested access" do
