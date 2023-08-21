b7d8f3054dd14238444a4b439e87ebd08776d708
Jason Draper <jason@drapergeek.com>
2023-08-21 16:29:47 -0400

    Add a cop to require custom errors

---
+++ lib/rubocop/cop/ezcater/require_custom_error.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module RuboCop
+  module Cop
+    module Ezcater
+      # Checks for `raise` on `StandardError` and `ArgumentError`.
+      # We want to be explicit about the error we're raising and use a custom error
+      #
+      # @example
+      #   # bad
+      #   raise StandardError, "You can't do that"
+      #
+      #   # good
+      #   raise OrderActionNotAllowed
+
+      class RequireCustomError < Cop
+        MSG = "Use a custom error class that inherits from StandardError when raising an exception"
+
+        def_node_matcher :raising_standard_or_argument_error,
+                         "(send nil? {:raise :fail} (const nil? {:StandardError :ArgumentError} ...) ...)"
+
+        def_node_matcher :initializing_standard_or_argument_error,
+                         "(send nil? {:raise :fail} (send (const nil? {:StandardError :ArgumentError} ...) ...))"
+
+        def on_send(node)
+          raising_standard_or_argument_error(node) do
+            add_offense(node, message: format(MSG))
+          end
+
+          initializing_standard_or_argument_error(node) do
+            add_offense(node, message: format(MSG))
+          end
+        end
+      end
+    end
+  end
+end
