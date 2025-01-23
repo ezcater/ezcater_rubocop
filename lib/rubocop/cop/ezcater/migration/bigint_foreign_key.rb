1f2602ebd06fe24e90f2a49391f1c141bf03ad61
Brian Ray <brian.ray@ezcater.com>
2025-01-23 10:41:06 -0500

    Check add_column and add_reference

--- lib/rubocop/cop/ezcater/migration/bigint_foreign_key.rb
+++ lib/rubocop/cop/ezcater/migration/bigint_foreign_key.rb
@@ -4,7 +4,7 @@ module RuboCop
   module Cop
     module Ezcater
       module Migration
-        # Use `#bigint` instead of `#integer` for all foreign keys.
+        # Use `bigint` instead of `integer` for all foreign keys.
         #
         # @example
         #
@@ -18,6 +18,18 @@ module RuboCop
         #     t.integer :bar_id, limit: 7
         #   end
         #
+        #   # bad
+        #   add_column :foos, :bar_id, :integer
+        #
+        #   # bad
+        #   add_column :foos, :bar_id, :integer, limit: 7
+        #
+        #   # bad
+        #   add_reference :foos, :bar, type: :integer
+        #
+        #   # bad
+        #   add_reference :foos, :bar, type: :integer, limit: 7
+        #
         #   # good
         #   create_table :foos do |t|
         #     t.bigint :bar_id
@@ -28,36 +40,121 @@ module RuboCop
         #     t.references :bar
         #   end
         #
+        #   # good
+        #   add_column :foos, :bar_id, :bigint
+        #
+        #   # good
+        #   add_reference :foos, :bar
+        #
         class BigintForeignKey < Base
-          MSG = "To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `#bigint` instead of `#integer`."
+          MSG = "To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`."
 
           def ends_with_id?(str)
             str.end_with?("_id")
           end
 
-          # @!method foreign_key_not_bigint?(node)
-          def_node_matcher :foreign_key_not_bigint?, <<~PATTERN
+          # @!method t_integer_method(node)
+          def_node_matcher :t_integer_method, <<~PATTERN
             (send
               # Any local variable that calls an `integer` method
               (lvar _) :integer
 
-              # The first argument is a symbol or string ending in _id
+              # Column name: symbol or string ending in _id
               {(sym #ends_with_id?) (str #ends_with_id?)}
 
-              # There's optionally a hash argument that includes a limit key
+              # Optional hash that includes a limit key
               (hash <(pair (sym :limit) (int $_)) ...>)?
             )
           PATTERN
 
+          # @!method t_references_method(node)
+          def_node_matcher :t_references_method, <<~PATTERN
+            (send
+              # Any local variable that calls a `references` or `belongs_to` method
+              (lvar _) {:references :belongs_to}
+
+              # Reference name
+              {(sym _) (str _)}
+
+              # A hash that includes :integer type
+              (hash <(pair (sym :type) (sym :integer)) ...>)
+            )
+          PATTERN
+
+          # @!method add_column_method(node)
+          def_node_matcher :add_column_method, <<~PATTERN
+            # A call to an `add_column` method
+            (send nil? :add_column
+              # Table name
+              {(sym _) (str _)}
+
+              # Column name: a symbol or string ending in _id
+              {(sym #ends_with_id?) (str #ends_with_id?)}
+
+              # Column type
+              (sym :integer)
+
+              # Optional hash that includes a limit key
+              (hash <(pair (sym :limit) (int $_)) ...>)?
+            )
+          PATTERN
+
+          # @!method add_reference_method(node)
+          def_node_matcher :add_reference_method, <<~PATTERN
+            # A call to a `add_reference` or `add_belongs_to` method
+            (send nil? {:add_reference :add_belongs_to}
+              # Table name
+              {(sym _) (str _)}
+
+              # Reference name
+              {(sym _) (str _)}
+
+              # A hash that includes :integer type
+              (hash <(pair (sym :type) (sym :integer)) ...>)
+            )
+          PATTERN
+
+          # @!method limit_pair(node)
+          def_node_search :limit_pair, <<~PATTERN
+            (pair (sym :limit) (int $_))
+          PATTERN
+
           def on_send(node)
-            foreign_key_not_bigint?(node) do |captures|
+            t_integer_method(node) do |captures|
               limit_value = captures.first
 
               if limit_value.nil? || limit_value < 8
                 add_offense(node)
               end
             end
+
+            t_references_method(node) do |capture|
+              limit_vals = limit_pair(node)
+              limit_value = limit_vals.first
+
+              if limit_value.nil? || limit_value < 8
+                add_offense(node)
+              end
+            end
+
+            add_column_method(node) do |captures|
+              limit_value = captures.first
+
+              if limit_value.nil? || limit_value < 8
+                add_offense(node)
+              end
+            end
+
+            add_reference_method(node) do
+              limit_vals = limit_pair(node)
+              limit_value = limit_vals.first
+
+              if limit_value.nil? || limit_value < 8
+                add_offense(node)
+              end
+            end
           end
+
         end
       end
     end
--- spec/rubocop/cop/ezcater/migration/bigint_foreign_key_spec.rb
+++ spec/rubocop/cop/ezcater/migration/bigint_foreign_key_spec.rb
@@ -1,79 +1,233 @@
 # frozen_string_literal: true
 
 RSpec.describe RuboCop::Cop::Ezcater::Migration::BigintForeignKey, :config do
-  it "registers an offense when adding a column ending in \"_id\" that's not a bigint (symbol name)" do
-    expect_offense(<<~RUBY)
-      create_table :foos do |t|
-        t.integer :bar_id
-        ^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `#bigint` instead of `#integer`.
-      end
-    RUBY
+  context "when adding a column using t.* methods" do
+    it "registers an offense when adding a column ending in \"_id\" that's not a bigint (symbol name)" do
+      expect_offense(<<~RUBY)
+        create_table :foos do |t|
+          t.integer :bar_id
+          ^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`.
+        end
+      RUBY
+    end
+
+    it "registers an offense when adding a column ending in \"_id\" that's not a bigint (string name)" do
+      expect_offense(<<~RUBY)
+        create_table :foos do |t|
+          t.integer "bar_id"
+          ^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`.
+        end
+      RUBY
+    end
+
+    it "registers an offense when adding a column ending in \"_id\" that specifies a limit < 8 as its only hash key" do
+      expect_offense(<<~RUBY)
+        create_table :foos do |t|
+          t.integer :bar_id, limit: 7
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`.
+        end
+      RUBY
+    end
+
+    it "registers an offense when adding a column ending in \"_id\" that specifies a limit < 8 as one of several hash keys" do
+      expect_offense(<<~RUBY)
+        create_table :foos do |t|
+          t.integer :bar_id, null: false, limit: 7
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`.
+        end
+      RUBY
+    end
+
+    it "registers an offense when adding a reference that specifies an :integer type" do
+      expect_offense(<<~RUBY)
+        create_table :foos do |t|
+          t.references :bar, type: :integer
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`.
+        end
+      RUBY
+    end
+
+    it "registers an offense when adding a reference that specifies an :integer type and a limit < 8" do
+      expect_offense(<<~RUBY)
+        create_table :foos do |t|
+          t.references :bar, type: :integer, limit: 7
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`.
+        end
+      RUBY
+    end
+
+    it "registers an offense when adding a belongs_to that specifies an :integer type" do
+      expect_offense(<<~RUBY)
+        create_table :foos do |t|
+          t.belongs_to :bar, type: :integer
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`.
+        end
+      RUBY
+    end
+
+    it "registers an offense when adding a belongs_to that specifies an :integer type and a limit < 8" do
+      expect_offense(<<~RUBY)
+        create_table :foos do |t|
+          t.belongs_to :bar, type: :integer, limit: 7
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`.
+        end
+      RUBY
+    end
+
+    it "does not register an offense when adding a column not ending in \"_id\"" do
+      expect_no_offenses(<<~RUBY)
+        create_table :foos do |t|
+          t.integer :bar
+        end
+      RUBY
+    end
+
+    it "does not register an offense when adding a column ending in \"_id\" that's a bigint" do
+      expect_no_offenses(<<~RUBY)
+        create_table :foos do |t|
+          t.bigint :bar_id
+        end
+      RUBY
+    end
+
+    it "does not register an offense when adding a column ending in \"_id\" that specifies a limit >= 8" do
+      expect_no_offenses(<<~RUBY)
+        create_table :foos do |t|
+          t.integer :bar_id, limit: 8
+        end
+      RUBY
+    end
+
+    it "does not register an offense when using #references" do
+      expect_no_offenses(<<~RUBY)
+        create_table :foos do |t|
+          t.references :bar
+        end
+      RUBY
+    end
+
+    it "does not register an offense when using #references with limit >= 8" do
+      expect_no_offenses(<<~RUBY)
+        create_table :foos do |t|
+          t.references :bar, type: :integer, limit: 8
+        end
+      RUBY
+    end
+
+    it "does not register an offense when using #belongs_to" do
+      expect_no_offenses(<<~RUBY)
+        create_table :foos do |t|
+          t.belongs_to :bar
+        end
+      RUBY
+    end
+
+    it "does not register an offense when using #belongs_to with limit >= 8" do
+      expect_no_offenses(<<~RUBY)
+        create_table :foos do |t|
+          t.belongs_to :bar, type: :integer, limit: 8
+        end
+      RUBY
+    end
   end
 
-  it "registers an offense when adding a column ending in \"_id\" that's not a bigint (string name)" do
-    expect_offense(<<~RUBY)
-      create_table :foos do |t|
-        t.integer "bar_id"
-        ^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `#bigint` instead of `#integer`.
-      end
-    RUBY
-  end
+  context "when adding a column using add_* methods" do
+    it "registers an offense when adding a column ending in \"_id\" that's not a bigint (symbol name)" do
+      expect_offense(<<~RUBY)
+        add_column :foos, :bar_id, :integer
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`.
+      RUBY
+    end
 
-  it "registers an offense when adding a column ending in \"_id\" that specifies a limit < 8 as its only hash key" do
-    expect_offense(<<~RUBY)
-      create_table :foos do |t|
-        t.integer :bar_id, limit: 7
-        ^^^^^^^^^^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `#bigint` instead of `#integer`.
-      end
-    RUBY
-  end
+    it "registers an offense when adding a column ending in \"_id\" that's not a bigint (string name)" do
+      expect_offense(<<~RUBY)
+        add_column :foos, "bar_id", :integer
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`.
+      RUBY
+    end
 
-  it "registers an offense when adding a column ending in \"_id\" that specifies a limit < 8 as one of several hash keys" do
-    expect_offense(<<~RUBY)
-      create_table :foos do |t|
-        t.integer :bar_id, null: false, limit: 7
-        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `#bigint` instead of `#integer`.
-      end
-    RUBY
-  end
+    it "registers an offense when adding a column ending in \"_id\" that specifies a limit < 8 as its only hash key" do
+      expect_offense(<<~RUBY)
+        add_column :foos, :bar_id, :integer, limit: 7
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`.
+      RUBY
+    end
 
-  it "does not register an offense when adding a column not ending in \"_id\"" do
-    expect_no_offenses(<<~RUBY)
-      create_table :foos do |t|
-        t.integer :bar
-      end
-    RUBY
-  end
+    it "registers an offense when adding a column ending in \"_id\" that specifies a limit < 8 as one of several hash keys" do
+      expect_offense(<<~RUBY)
+        add_column :foos, :bar_id, :integer, null: false, limit: 7
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`.
+      RUBY
+    end
 
-  it "does not register an offense when adding a column ending in \"_id\" that's a bigint" do
-    expect_no_offenses(<<~RUBY)
-      create_table :foos do |t|
-        t.bigint :bar_id
-      end
-    RUBY
-  end
+    it "registers an offense when adding a reference that specifies an :integer type" do
+      expect_offense(<<~RUBY)
+        add_reference :foos, :bar, type: :integer
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`.
+      RUBY
+    end
 
-  it "does not register an offense when adding a column ending in \"_id\" that specifies a limit >= 8" do
-    expect_no_offenses(<<~RUBY)
-      create_table :foos do |t|
-        t.integer :bar_id, limit: 8
-      end
-    RUBY
-  end
+    it "registers an offense when adding a reference that specifies an :integer type and a limit < 8" do
+      expect_offense(<<~RUBY)
+        add_reference :foos, :bar, type: :integer, limit: 7
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`.
+      RUBY
+    end
 
-  it "does not register an offense when adding a foreign key column using #belongs_to" do
-    expect_no_offenses(<<~RUBY)
-      create_table :foos do |t|
-        t.belongs_to :bar
-      end
-    RUBY
-  end
+    it "registers an offense when adding a belongs_to that specifies an :integer type" do
+      expect_offense(<<~RUBY)
+        add_belongs_to :foos, :bar, type: :integer
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`.
+      RUBY
+    end
 
-  it "does not register an offense when adding a foreign key column using #references" do
-    expect_no_offenses(<<~RUBY)
-      create_table :foos do |t|
-        t.references :bar
-      end
-    RUBY
+    it "registers an offense when adding a belongs_to that specifies an :integer type and a limit < 8" do
+      expect_offense(<<~RUBY)
+        add_belongs_to :foos, :bar, type: :integer, limit: 7
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ To prevent foreign keys from potentially running out of int values before their referenced primary keys, use `bigint` instead of `integer`.
+      RUBY
+    end
+
+    it "does not register an offense when adding a column not ending in \"_id\"" do
+      expect_no_offenses(<<~RUBY)
+        add_column :foos, :bar, :integer
+      RUBY
+    end
+
+    it "does not register an offense when adding a column ending in \"_id\" that's a bigint" do
+      expect_no_offenses(<<~RUBY)
+        add_column :foos, :bar_id, :bigint
+      RUBY
+    end
+
+    it "does not register an offense when adding a column ending in \"_id\" that specifies a limit >= 8" do
+      expect_no_offenses(<<~RUBY)
+        add_column :foos, :bar_id, :integer, limit: 8
+      RUBY
+    end
+
+    it "does not register an offense when using #add_reference" do
+      expect_no_offenses(<<~RUBY)
+        add_reference :foos, :bar
+      RUBY
+    end
+
+    it "does not register an offense when using #add_reference with limit >= 8" do
+      expect_no_offenses(<<~RUBY)
+        add_reference :foos, :bar, type: :integer, limit: 8
+      RUBY
+    end
+
+    it "does not register an offense when using #add_belongs_to" do
+      expect_no_offenses(<<~RUBY)
+        add_belongs_to :foos, :bar
+      RUBY
+    end
+
+    it "does not register an offense when using #add_belongs_to with limit >= 8" do
+      expect_no_offenses(<<~RUBY)
+        add_belongs_to :foos, :bar, type: :integer, limit: 8
+      RUBY
+    end
   end
 end
